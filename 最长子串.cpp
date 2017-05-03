f298f53951d1cc875c32f67fabee743276f4b695
Touko <HoshinoTouko@users.noreply.github.com>
2017-05-04 01:24:15 +0800

    Update 最长子串.cpp

--- 最长子串.cpp
+++ 最长子串.cpp
@@ -5,24 +5,24 @@ int main() {
 	int a[] = { -6, 2, 4, -7, 5, 3, 2, -1, 6, -9, 10, -2 };
 	int n = sizeof(a)/sizeof(int);
 	int p1, p2, start, end, max=1<<(sizeof(int)-1), t;
-	
+
 	for (p1 = 0; p1 < n; p1++) {
-	    while(a[p1] < 0 && p1 < n) 
-            p1++;
-	    if (p1 == n) 
-            break;
-        t = 0;
-        for (p2 = p1; p2 < n; p2++){
-            t += a[p2];
-            if (t > max){
-                start = p1;
-                end = p2;
-                max = t;
-            }
-        }
-    }
-	
+		while(a[p1] < 0 && p1 < n)
+			p1++;
+		if (p1 == n)
+			break;
+		t = 0;
+		for (p2 = p1; p2 < n; p2++) {
+			t += a[p2];
+			if (t > max) {
+				start = p1;
+				end = p2;
+				max = t;
+			}
+		}
+	}
+
 	cout<<"Max Value: "<<max<<" Start at: "<<start<<" End at: "<<end;
-	
+
 	return 0;
 }
